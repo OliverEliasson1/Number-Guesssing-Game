47a6789d03a76f7ee88026b4fc5342d3c32eb64a
mcsnoopys <olivereliasson1@gmail.com>
2026-09-12 19:28:36 +0200

    Bare minimum guessing game

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main()
     {
+        int secretNr = 25;
+        Console.WriteLine("Gissa på ett heltal mellan 1-50");
+        var guess = Console.ReadLine();
+        if(!int.TryParse(guess, out int value))
+        {
+            Console.WriteLine("Du måste ange ett heltalsvärde");
+        }
+        else if(value == secretNr)
+        {
+            Console.WriteLine("Du gissade rätt!");
+        }
         
     }
 }
