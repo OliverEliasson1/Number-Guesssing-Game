8626f231d26bfa4a33b28693fb97389e56c57355
mcsnoopys <olivereliasson1@gmail.com>
2026-09-12 19:55:34 +0200

    Put the game in a loop and added random Int

--- Program.cs
+++ Program.cs
@@ -4,17 +4,30 @@ class Program
 {
     static void Main()
     {
-        int secretNr = 25;
-        Console.WriteLine("Gissa på ett heltal mellan 1-50");
-        var guess = Console.ReadLine();
-        if(!int.TryParse(guess, out int value))
-        {
-            Console.WriteLine("Du måste ange ett heltalsvärde");
-        }
-        else if(value == secretNr)
-        {
-            Console.WriteLine("Du gissade rätt!");
-        }
+        Random rnd = new Random();
+        int secretNr = rnd.Next(1,51);
         
+        while (true)
+        {
+            Console.WriteLine("Gissa på ett heltal mellan 1-50");
+            var guess = Console.ReadLine();
+            if(!int.TryParse(guess, out int value))
+            {
+                Console.WriteLine("Du måste ange ett heltalsvärde");
+            }
+            else if(value < secretNr)
+            {
+                Console.WriteLine("Numret är för lågt");
+            }
+            else if(value > secretNr)
+            {
+                Console.WriteLine("Numret är för högt");
+            }
+            else
+            {
+                Console.WriteLine("Du gissade rätt!");
+                break;
+            }
+        }
     }
 }
